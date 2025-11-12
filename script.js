a3ddbc83da84d7ee69f50e11df0064621fbfd230
Caroline Vézina <caroline_vezina@hotmail.com>
2025-11-12 12:51:20 -0500

    mobile version

---
+++ script.js
@@ -0,0 +1,15 @@
+// ########### BURGER MENU ########### //
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header-right");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("menu-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
